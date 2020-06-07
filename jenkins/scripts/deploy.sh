fe6bbb3ac3987f5f679d68ff2370ffcf49ac3e35
digitaalmenu <66532667+digitaalmenu@users.noreply.github.com>
2020-06-07 15:52:04 +0200

    Update deploy.sh

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,8 +1,3 @@
 #!/bin/sh
-ssh louismylle@192.168.1.183 <<EOF
- cd /home/louismylle/digitaalmenu/site/creating-a-pipeline-in-blue-ocean/
- git pull
- npm install — production
- npm run build
- exit
-EOF
+ssh louismylle@192.168.1.183 'cd /home/louismylle/digitaalmenu/site/creating-a-pipeline-in-blue-ocean/ && git pull && npm install — production && npm run build'
+exit
